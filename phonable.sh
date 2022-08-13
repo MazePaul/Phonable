d1b357314467a93e891bf51018d549649a21523a
mazepaul <a.saillour@protonmail.com>
2022-08-14 00:03:37 +0200

    Phonable_2_adding_packageinstall_termconfig

--- phonable.sh
+++ phonable.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
+List_packages_to_install=("bash" "curl" "gnupg" "snap" "snapd-xdg-open" "bsdutils" "build-essential" "darktable" "findutils" "gh" "gpick" "grep" "htop" "net-tools" "powertop" "resolvconf" "ssh" "steam-installer" "sublime-text" "telegram-desktop" "tlp" "vim" "virt-manager" "wireguard")
+List_packages_to_install_snap=("discord" "bitwarden" "brave" "intellij-idea-community --classic")
+
+function install_package {
+  for Package in ${$1[@]};
+    do
+      PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $Package | grep "install ok installed")
+      echo Checking for $1[@]: $PKG_OK
+      if [ "" = "$PKG_OK" ]; then
+        echo "No $Package. Setting up $Package."
+        $2 $Package
+      fi
+    done
+}
+
+function terminal_config {
+  echo "Choose Konsole terminal emulator"
+  sudo update-alternatives --config x-terminal-emulator
+  rm ~/.kde/share/config/kdeglobals
+  cp kdeglobals ~/.kde/share/config/kdeglobals
+  source ~/.bashrc
+}
+
 function main_installation {
-  echo "main_installation"
+  install_package $"List_packages_to_install" $"sudo apt-get --yes install $Package"
+  install_package $"List_packages_to_install_snap" $"sudo snap install $Package"
+  sudo apt-get -f install
+
+  terminal_config
 }
 
 function vm_pentest {
