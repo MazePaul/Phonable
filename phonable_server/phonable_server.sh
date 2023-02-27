103d78ecf3b29f225c7ac433656c0fcde2190cc9
mazepaul <a.saillour@protonmail.com>
2023-02-27 20:18:28 +0100

    Creation phonable server

---
+++ phonable_server/phonable_server.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function_user{
+echo "User set up\n"
+sudo apt-get update && sudo apt-get upgrade
+
+read -p "Enter user: " user
+sudo useradd $user
+sudo usermod -aG sudo $user
+
+echo "VIM installation + alias"
+sudo apt-get install vim
+alias="alias vi='vim'"
+echo -e "$alias" >> ~/.bashrc
+source ~/.bashrc
+}
+
+function_firewall{
+echo "Firewall installation"
+
+sudo apt-get install ufw
+sudo ufw allow ssh
+sudo ufw enable
+}
+
+function_ssh{
+FILE=~/.ssh/authorized_keys
+if [-f "$FILE" ] t; then
+	mv sshd_config /etc/ssh/sshd_config
+else
+	echo "Key missing for ssh connection"
+	echo "commands: ssh-keygen -t rsa -b 4096 && ssh-copy-id -i public_key.pub user@ip"
+fi
+}
+
+function_fail2ban{
+mv jail.conf /etc/fail2ban/jail.conf
+}
